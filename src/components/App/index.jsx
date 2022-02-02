11f48881c594a1589c5b0e44058a3d8a6880893f
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2022-02-02 11:12:03 +0100

    02 Routování

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
-    <Header />
-    <Home />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservation/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+export const Reservation = () => {
+  return (
+    <h2>Detail jízdenky</h2>
+  )
+  };
