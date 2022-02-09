c3ffef83b0949c0cd8ee62fd24c9ced5371ed0ed
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2022-02-09 21:05:49 +0100

    11_Potvrzení rezervace

--- src/components/SeatPicker/index.jsx
+++ src/components/SeatPicker/index.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { SeatRow } from '../SeatRow';
 import './style.css';
 
 export const SeatPicker = ({ seats, journeyId }) => {
-  const [selectedSeatNumber, setSelectedSeatNumber] = useState(null)
+  const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
 
   const handleSeatSelect = (number) => {
-    setSelectedSeatNumber(number)
+    setSelectedSeatNumber(number);
+  };
+
+ const navigate = useNavigate();
+
+  const handleBuy = () => {
+    fetch(
+      `https:/leviexpress-backend.herokuapp.com/api/reserve?seat=${selectedSeatNumber}&journeyId=${journeyId}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then((resp) => resp.json())
+      .then((json) => navigate(`/reservation/${json.data.reservationId}`));
   };
 
   return (
@@ -14,10 +31,20 @@ export const SeatPicker = ({ seats, journeyId }) => {
       <h2>Vyberte sedadlo</h2>
       <div className="seats">
         {seats.map((row, index) => (
-          <SeatRow row={row} onSeatSelected={handleSeatSelect} selectedSeatNumber={selectedSeatNumber} key={index} />
+          <SeatRow
+            row={row}
+            onSeatSelected={handleSeatSelect}
+            selectedSeatNumber={selectedSeatNumber}
+            key={index}
+          />
         ))}
       </div>
-      <button className="btn" type="button">
+      <button
+        className="btn"
+        type="button"
+        disabled={selectedSeatNumber === null}
+        onClick={handleBuy}
+      >
         Rezervovat
       </button>
     </div>
